a0d721aae50fe15b56e4cbce2498ffb06b537c8b
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-06-16 23:25:45 +0300

    scrol

--- src/index.js
+++ src/index.js
@@ -73,3 +73,15 @@ function displaysTotalHids() {
   stopsMarkapPage(newApiService.hits);
   console.log(newApiService.hits);
 }
+
+const scrollEl = document.querySelector('.js-scroll');
+
+scrollEl.addEventListener('click', () => window.scroll(0, 0));
+window.addEventListener('scroll', onScrollClick);
+
+function onScrollClick(e) {
+  e.preventDefault();
+  window.scrollY < 320
+    ? scrollEl.classList.remove('isShown')
+    : scrollEl.classList.add('isShown');
+}
